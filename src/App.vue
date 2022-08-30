41fc72c886e7765fabbc88d1aabc40dd635be7d1
Cera Kocher <cerakocher@Ceras-Air.home>
2022-08-30 16:32:12 +0200

    created project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id = "app">
+        <my-users />
+    </div>
+</template>
+
+<script>
+import MyUsers from './components/MyUsers.vue';
+
+export default {
+  name: 'app',
+  components: {
+    MyUsers,
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyUsers.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+    <h3>Users:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in myusers" v-bind:key="user.id"> 
+          <th scope="row">{{user.id}}</th>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.address.city}}</td>
+        </tr>
+      </tbody>
+    </table> 
+  </div> 
+</template>
+
+
+<script>
+    import axios from 'axios';
+
+    export default{
+        name: 'MyUsers',
+        data(){
+            return {
+                myusers: null,
+            };
+        },
+        created: function() {
+            axios
+                .get('https://jsonplaceholder.typicode.com/users/')
+                .then(res => {
+          this.myusers = res.data;
+            })
+        }
+    }
+</script>
+
+<styles>
+</styles>
